e95961fae42349e3f753f93f56faaf983379697a
Yaktas <joswhitestone@gmail.com>
2020-12-28 23:38:42 -0600

    new after conflict

--- src/test/java/gmiBank_team22/pojo/PojoDeneme.java
+++ src/test/java/gmiBank_team22/pojo/PojoDeneme.java
@@ -4,6 +4,6 @@ public class PojoDeneme {
     public static void main(String[] args) {
         System.out.println("PojoDenem");
         System.out.println("deneme2Pjofrom Yucel");
-
+        System.out.println("hello after conflict");
     }
 }
