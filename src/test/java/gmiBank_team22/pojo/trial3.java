65383d85efc9979d6fb62df42001a551302b22eb
mihrac5545samsun <mihracgyahoo.com>
2020-12-28 23:56:40 -0600

    hh

--- src/test/java/gmiBank_team22/pojo/trial3.java
+++ src/test/java/gmiBank_team22/pojo/trial3.java
@@ -1,7 +1,7 @@
 package gmiBank_team22.pojo;
 
-public class Trial2 {
+public class trial3 {
     public static void main(String[] args) {
-        System.out.println("iiii");
+        System.out.println("oooo");
     }
 }
