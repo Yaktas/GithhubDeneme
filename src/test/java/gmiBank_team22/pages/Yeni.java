4004a86bf2773e4fc538ed1e8c6d18bbdbd20ee2
Yaktas <joswhitestone@gmail.com>
2020-12-29 00:03:45 -0600

    yeniyeni

--- src/test/java/gmiBank_team22/pages/Yeni.java
+++ src/test/java/gmiBank_team22/pages/Yeni.java
@@ -3,5 +3,6 @@ package gmiBank_team22.pages;
 public class Yeni {
     public static void main(String[] args) {
         System.out.println("yeni");
+        System.out.println("benim yeni from yucel");
     }
 }
