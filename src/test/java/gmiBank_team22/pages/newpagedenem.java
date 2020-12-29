f4bfaff0cbe782dde3ab13d394e6205c4bf3377b
Yaktas <joswhitestone@gmail.com>
2020-12-28 23:55:21 -0600

    hello

--- src/test/java/gmiBank_team22/pages/newpagedenem.java
+++ src/test/java/gmiBank_team22/pages/newpagedenem.java
@@ -3,6 +3,7 @@ package gmiBank_team22.pages;
 public class newpagedenem {
     public static void main(String[] args) {
         System.out.println("deneme");
+        System.out.println("deneme 2");
     }
 
 }
